3ddb9321b130dba6b3af93cf103913f97ddedf5a
e04 <47185462+e04@users.noreply.github.com>
2023-11-15 23:03:14 +0900

    Initial Commit

---
+++ zimac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zimac
+//
+//  Created by a on 2023/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zimac/zimacApp.swift
@@ -0,0 +1,17 @@
+//
+//  zimacApp.swift
+//  zimac
+//
+//  Created by a on 2023/11/15.
+//
+
+import SwiftUI
+
+@main
+struct zimacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
